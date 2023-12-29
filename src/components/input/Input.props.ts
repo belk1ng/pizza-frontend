6edbdcb30b9774e233aa2f2817476a012411cb5e
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-12-29 21:00:50 +0300

    refactor: 💡 Изменен компонент Input
    Добавлен пропс valid для подсвечивания инпута в случае ошибки его
    заполнения.

--- src/components/input/Input.props.ts
+++ src/components/input/Input.props.ts
@@ -3,4 +3,5 @@ import { InputHTMLAttributes, ReactNode } from "react";
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   startIcon?: ReactNode;
   label?: string;
+  valid?: boolean;
 }
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,13 +1,24 @@
 import { FC } from "react";
 import { InputProps } from "./Input.props.ts";
 import classes from "./Input.module.css";
+import cn from "classnames";
 
-const Input: FC<InputProps> = ({ label, startIcon, ...props }) => {
+const Input: FC<InputProps> = ({
+  label,
+  startIcon,
+  valid = true,
+  ...props
+}) => {
   return (
     <label className={classes.input}>
       {startIcon && <div>{startIcon}</div>}
       {label && <span className={classes.input__label}>{label}</span>}
-      <input className={classes.input__field} {...props} />
+      <input
+        className={cn(classes.input__field, {
+          [classes["input__field--invalid"]]: !valid,
+        })}
+        {...props}
+      />
     </label>
   );
 };
