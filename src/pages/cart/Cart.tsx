cd4b65a45e231d157f963a8838da7263698602d6
belk1ng <dmitriy.belkin.2001@mail.ru>
2024-01-20 12:58:20 +0300

    feat: 🎸 Добавлен подсчет итоговой суммы заказа

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useCallback } from "react";
 import axiosInstance from "@/api/axios";
 import { Product, Products } from "@/types/product";
 import CartItem from "@components/cart-item";
+import Button from "@components/ui/button";
 import Heading from "@components/ui/heading";
 import withTitle from "@hocs/withTitle";
 import { useAppSelector } from "@store/hooks";
@@ -15,7 +16,7 @@ const Cart = () => {
 
   const [productIds, setProductIds] = useState("");
 
-  const { products } = useAppSelector(cartSelector);
+  const { products, productsCount } = useAppSelector(cartSelector);
 
   useEffect(() => {
     setProductIds(Object.keys(products).join(","));
@@ -61,6 +62,13 @@ const Cart = () => {
     };
   }, [productIds, loadItems]);
 
+  const DELIVERY_PRICE = items.length === 0 ? 0 : 300;
+
+  const PRODUCTS_PRICE = items.reduce(
+    (acc, curr) => acc + curr.price * products[curr.id],
+    0
+  );
+
   const renderItems = () => {
     if (items.length === 0) {
       return <p className={classes.section__message}>Ваша корзина пуста...</p>;
@@ -84,7 +92,35 @@ const Cart = () => {
         <Heading>Корзина</Heading>
       </header>
       <section className={classes.section}>
-        <ul className={classes.section__list}>{renderItems()}</ul>
+        <ul className={classes["section__items-list"]}>{renderItems()}</ul>
+        <ul className={classes["section__price-list"]}>
+          <li className={classes.section__price}>
+            <span>
+              Сумма заказа
+              <span className={classes.section__count}>({productsCount})</span>
+            </span>
+            <span>
+              {PRODUCTS_PRICE} <span className={classes.section__ruble}>₽</span>
+            </span>
+          </li>
+          <li className={classes.section__price}>
+            <span>Доставка</span>
+            <span>
+              {DELIVERY_PRICE} <span className={classes.section__ruble}>₽</span>
+            </span>
+          </li>
+          <li className={classes.section__price}>
+            <span>Итог</span>
+            <span>
+              {PRODUCTS_PRICE + DELIVERY_PRICE}{" "}
+              <span className={classes.section__ruble}>₽</span>
+            </span>
+          </li>
+        </ul>
+
+        <Button size="large" className={classes.section__button}>
+          Оформить
+        </Button>
       </section>
     </>
   );
